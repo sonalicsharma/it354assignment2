fbf6c54fca97b00bafd5d2bc28b7390482e1424c
sonalicsharma <sonalicsharma@yahoo.com>
2016-02-13 11:42:12 -0600

    Creating empty customer_view.php file

---
+++ customer_view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <!-- Website Title & Description for Search Engine purposes -->
+        <title>View customer details</title>
+        <meta name="description" content="">
+
+        <!-- Mobile viewport optimized -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+        <!-- Bootstrap CSS -->
+        <link href="includes/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>   
+
+        <!-- Custom CSS -->
+        <link href="includes/css/styles.css" rel="stylesheet">  
+    </head>
+    <body>
+        Hello world!
+    </body>
+</html>
