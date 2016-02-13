3b82c7074e253d5e8802226952ea9aac668adeaf
Nandita Ramakrishnan <nandita.ramakrishnan28@gmail.com>
2016-02-13 12:01:30 -0600

    Added php file for customer add

---
+++ customer_add.php
@@ -0,0 +1,33 @@
+<?php
+$hn = 'www.it354.com';
+$db = 'it354_students';
+$un = 'it354_students';
+$pw = 'steinway';
+$conn = new mysqli($hn, $un, $pw, $db);
+if ($conn->connect_error) die($conn->connect_error);
+$fname = $_POST["fname"];
+$lname = $_POST['lname'];
+$addr = $_POST['address'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$zip = $_POST['zip'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$sql="INSERT INTO customers(firstName, lastName, address, city, state, zip, email, phone )
+VALUES('$fname','$lname', '$addr', '$city', '$state', '$zip', '$email', '$phone')";
+if ($conn->query($sql) === TRUE) {
+    echo "<br/><br/><span>New customer details added successfully!!</span>";
+} else {
+    echo "<br/><br/><span>Error while inserting customer details!!</span>";;
+}
+
+$conn->close();
+//header("Location:customer_add.html?result=1",true, 301);
+//exit;
+?>
+
+<html>
+    <body>
+        &nbsp;&nbsp;<button type="submit" class="btn btn-default" onclick="location.href='customer_add.html'">Back</button>
+    </body>
+</html>
