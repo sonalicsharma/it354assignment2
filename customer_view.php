3dfea941172ade39d3bfd82eee6cf3cf8417ee32
sonalicsharma <sonalicsharma@yahoo.com>
2016-02-13 12:23:05 -0600

    Added navbar tabs

--- customer_view.php
+++ customer_view.php
@@ -10,16 +10,31 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
         <!-- Bootstrap CSS -->
-        <link href="includes/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>   
+        <link href="dist/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>   
 
         <!-- Custom CSS -->
-        <link href="includes/css/styles.css" rel="stylesheet">  
+        <link href="dist/css/styles.css" rel="stylesheet">  
     </head>
     <body>
-        Hello world!
+        <div class="container" id="main">
+            <div class="navbar navbar-fixed-top navbar-default">
+                <div class="container-fluid">
+                    <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+                    <button class="navbar-toggle" data-target=".navbar-responsive-collapse" data-toggle="collapse" type="button">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Assignment 2</a>
+                    <div class="navbar-collapse collapse navbar-responsive-collapse">
+                        <ul class="nav navbar-nav">
+
+                            <li class = "active"><a>View</a></li>
+                            <li><a href = "customer_add.html">Add</a></li>
+                        </ul>
+                    </div><!-- end nav-responsive-collapse -->
+                </div><!-- end container -->
+            </div><!-- end navbar -->
+        </div>
     </body>
 </html>
-<<<<<<< cfc007b7fedd820870d91c27e6581d6139f9e7b6
-=======
-
->>>>>>> Cleaned up customer_view.php
